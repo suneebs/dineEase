a44e71c515623da870af0a2a7d9b9a5177a2f9ac
suneeb <ssuneebvishnu@gmail.com>
2024-09-21 16:24:49 +0530

    fix:nav, cart , toggle function

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,10 +3,10 @@ import menu from "@/assets/menu.png";
 import Search from "./CarouselandSearch";
 import MenuItems from "./MenuItems";
 import { useCart } from './cartToggle';
-// import DrawerDemo  from './Drawer';
+
 const Layout = () => {
 
-    const { isCartVisible, toggleCart,cart } = useCart();
+    const { isCartVisible, toggleCart } = useCart();
 
     return (
         <div className="flex">
@@ -24,25 +24,10 @@ const Layout = () => {
             </main>
             <section className={`fixed top-0 right-0 bg-gray-800 shadow-2xl h-full w-60 md:w-80 grid grid-rows-[60px_1fr_60px] transform ${isCartVisible ? "translate-x-0" : "translate-x-full"} transition-transform duration-300`}>
                 <div className="text-white text-center">CART</div>
-                {/* Add cart content here */
-                cart.length === 0 ? (
-                    <div className='p-10 h-8 flex justify-center pt-72'>
-                        <p className='text-white font-bold font-serif'>Order list is empty</p>
-                    </div>
-                ) : (
-                    <div className='pt-10'>
-                        
-                        {cart.map((item, index) => (
-                            <div className='bg-white m-1 p-3 rounded-2xl font-serif' key={index}>
-                                {item.title} - ₹ {item.rate} {index}
-                            </div>
-                        ))}
-                    </div>
-                )}
-                {/* <div className="flex justify-center items-center text-white">Your cart is empty</div>
-                <div className="text-center text-white">Checkout</div> */}
+                {/* Add cart content here */}
+                <div className="flex justify-center items-center text-white">Your cart is empty</div>
+                <div className="text-center text-white">Checkout</div>
             </section>
-            
         </div>
     );
 }
--- src/components/cartToggle.jsx
+++ src/components/cartToggle.jsx
@@ -8,18 +8,13 @@ export const useCart = () => {
 
 export const CartProvider = ({ children }) => {
     const [isCartVisible, setIsCartVisible] = useState(false);
-    // Add to cart
-    const [cart, setCart] = useState([]);
-    const addToCart = (item) => {
-        setCart((prevCart) => [...prevCart, item]);
-    };
 
     const toggleCart = () => {
         setIsCartVisible((prev) => !prev);
     };
 
     return (
-        <CartContext.Provider value={{ isCartVisible, toggleCart, cart, addToCart }}>
+        <CartContext.Provider value={{ isCartVisible, toggleCart }}>
             {children}
         </CartContext.Provider>
     );
